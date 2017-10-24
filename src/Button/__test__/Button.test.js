f35659fcd6bdf95ff1416e9b939eb640ca0df11a
Denis Florkin <denisflorkin@gmail.com>
2017-10-24 12:57:34 +0200

    lint + add lint rules

--- src/Button/__test__/Button.test.js
+++ src/Button/__test__/Button.test.js
@@ -4,7 +4,8 @@ import { mount } from '../../../test/utils';
 
 it('should render a Button', () => {
   const testValue = 'YO!';
-  const wrapper = shallow(mount(<Button onClick={()=>{}} text={testValue}/>));
+  const wrapper = shallow(mount(<Button onClick={() => {}} text={testValue} />));
   expect(wrapper).toMatchSnapshot();
   expect(wrapper.props().text).toContain(testValue);
 });
+
