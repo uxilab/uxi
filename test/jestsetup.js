f0c0202ac9ea8d18a93fbb4472d38b19590eb175
Denis Florkin <denisflorkin@gmail.com>
2017-10-24 14:58:50 +0200

    fix raf test env polyfill warning

--- test/jestsetup.js
+++ test/jestsetup.js
@@ -1,5 +1,8 @@
 // Make Enzyme functions available in all test files without importing
 import { render, mount, shallow, configure } from 'enzyme';
+/* eslint-disable import/first */
+/* eslint-disable  no-unused-vars */
+import raf from './raf';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
