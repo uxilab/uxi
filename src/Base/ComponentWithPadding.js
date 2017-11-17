4f91a113b3a4fcdbad83e7c2b30e302217c744f9
Denis Florkin <denisflorkin@gmail.com>
2017-11-17 14:05:54 +0100

    fix DivPadding

---
+++ src/Base/ComponentWithPadding.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class ComponentWithPadding extends Component {
+
+  static propTypes = {
+    margin: PropTypes.oneOf(['xs', 's', 'm', 'l', 'XS', 'S', 'M', 'L']),
+    padding: PropTypes.oneOf(['xs', 's', 'm', 'l', 'XS', 'S', 'M', 'L']),
+    style: PropTypes.object,
+  };
+
+  getMargin() {
+    const { margin } = this.props;
+
+    if (!margin) {
+      return {};
+    }
+
+    if (margin.toLowerCase() === 'xs') {
+      return {
+        margin: '5px',
+      };
+    }
+
+    if (margin.toLowerCase() === 's') {
+      return {
+        margin: '15px',
+      };
+    }
+
+    if (margin.toLowerCase() === 'm') {
+      return {
+        margin: '30px',
+      };
+    }
+
+    if (margin.toLowerCase() === 'l') {
+      return {
+        margin: '60px',
+      };
+    }
+
+    return {};
+  }
+
+  getPadding() {
+    const { padding } = this.props;
+
+    if (!padding) {
+      return {};
+    }
+
+    if (padding.toLowerCase() === 'xs') {
+      return {
+        padding: '5px',
+      };
+    }
+
+    if (padding.toLowerCase() === 's') {
+      return {
+        padding: '15px',
+      };
+    }
+
+    if (padding.toLowerCase() === 'm') {
+      return {
+        padding: '30px',
+      };
+    }
+
+    if (padding.toLowerCase() === 'l') {
+      return {
+        padding: '60px',
+      };
+    }
+
+    return {};
+  }
+
+  getStyle(stylesFromComponent = {}) {
+    const { style } = this.props;
+
+    return Object.assign({}, (style || {}), this.getPadding(), this.getMargin(), stylesFromComponent);
+  }
+}
+
+
+export default ComponentWithPadding;
--- src/Base/DivPadding.js
+++ src/Base/DivPadding.js
@@ -1,86 +1,16 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import ComponentWithPadding from './ComponentWithPadding';
 
-class ComponentWithPadding extends Component {
-  getMargin() {
-    const { margin } = this.props;
+class DivPadding extends ComponentWithPadding {
+  render() {
+    const { className, children } = this.props;
 
-    if (!margin) {
-      return {};
-    }
-
-    if (margin.toLowerCase() === 'xs') {
-      return {
-        margin: '5px',
-      };
-    }
-
-    if (margin.toLowerCase() === 's') {
-      return {
-        margin: '15px',
-      };
-    }
-
-    if (margin.toLowerCase() === 'm') {
-      return {
-        margin: '30px',
-      };
-    }
-
-    if (margin.toLowerCase() === 'l') {
-      return {
-        margin: '60px',
-      };
-    }
-
-    return {};
-  }
-
-  getPadding() {
-    const { padding } = this.props;
-
-    if (!padding) {
-      return {};
-    }
-
-    if (padding.toLowerCase() === 'xs') {
-      return {
-        padding: '5px',
-      };
-    }
-
-    if (padding.toLowerCase() === 's') {
-      return {
-        padding: '15px',
-      };
-    }
-
-    if (padding.toLowerCase() === 'm') {
-      return {
-        padding: '30px',
-      };
-    }
-
-    if (padding.toLowerCase() === 'l') {
-      return {
-        padding: '60px',
-      };
-    }
-
-    return {};
-  }
-
-  getStyle(stylesFromComponent = {}) {
-    const { style } = this.props;
-
-    return Object.assign({}, (style || {}), this.getPadding(), this.getMargin(), stylesFromComponent);
+    return (
+      <div className={className} style={this.getStyle()}>
+        {children}
+      </div>
+    );
   }
 }
 
-ComponentWithPadding.propTypes = {
-  margin: PropTypes.oneOf(['xs', 's', 'm', 'l', 'XS', 'S', 'M', 'L']),
-  padding: PropTypes.oneOf(['xs', 's', 'm', 'l', 'XS', 'S', 'M', 'L']),
-  style: PropTypes.object,
-};
-
-export default ComponentWithPadding;
+export default DivPadding;
