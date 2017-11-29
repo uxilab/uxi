ede0bf8196fb7675117cb77e6ba060fbbae77951
Denis Florkin <denisflorkin@gmail.com>
2017-11-29 15:02:03 +0100

    fix webapck config

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     'react-hot-loader/patch',
     // activate HMR for React
 
-    'webpack-dev-server/client?http://localhost:3000',
+    'webpack-dev-server/client?http://localhost:3001',
     // bundle the client for webpack-dev-server
     // and connect to the provided endpoint
 
