0ca7efce2252c41f52fe72b652011d681bc68997
Denis Florkin <denisflorkin@gmail.com>
2017-11-29 11:09:34 +0100

    extend errorr text to accept styles as props : https://www.pivotaltracker.com/story/show/153113848

--- src/Text/ErrorText.js
+++ src/Text/ErrorText.js
@@ -6,8 +6,8 @@ console.log(palette);
 const styles = {
   color: palette.semantic.error,
 };
-const ErrorText = ({ inline, children, text }) => (inline
-  ? <span style={styles}> { text || children } </span>
-  : <div style={styles}> { text || children } </div>);
+const ErrorText = ({ inline, children, text, style }) => (inline
+  ? <span style={{ ...styles, ...style }}> { text || children } </span>
+  : <div style={{ ...styles, ...style }}> { text || children } </div>);
 
 export default ErrorText;
