6b376aedb5445ff499d978888090688656bdb262
Denis Florkin <denisflorkin@Deniss-iMac.local>
2017-12-21 17:28:37 +0100

    fix react prop types import

--- src/Layout/LayoutBuilder.js
+++ src/Layout/LayoutBuilder.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Tab, TabList, TabPanel, Tabs } from '../Tabs';
 import Col from './Col';
 import Row from './Row';
--- src/Layout/LayoutProvider.js
+++ src/Layout/LayoutProvider.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Style } from 'radium';
 import LayoutObject from './Layout.style';
 
