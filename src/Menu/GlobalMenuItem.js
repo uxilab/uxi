c788af3ce62d43b5932bef063f32119311166bde
dflo <denisflorkin@gmail.com>
2018-02-10 13:52:53 +0100

    fix typo

--- src/Menu/GlobalMenuItem.js
+++ src/Menu/GlobalMenuItem.js
@@ -36,13 +36,13 @@ const GlobalMenuItemDiv = styled.div`
   position: relative;
   border-right: ${({ isSelected }) => (isSelected ?
     `${borderThickness} solid #0ea4a5` : `${borderThickness} solid transparent`)
-  }
+  };
   background: ${({ isSelected }) => (isSelected ? '#1b3c4f' : 'none')};
   transition: ${({ theme: { transition } }) => transition.defaultAll};
   &:hover {
     background: ${({ isSelected, theme: { palette } }) =>
     (isSelected ? palette.primary.main : palette.primary.light)
-  }
+  };
     *:not(svg) {
       color: ${({ theme: { palette } }) => palette.pureWhite};
       transition: ${({ theme: { transition } }) => transition.defaultAll};
