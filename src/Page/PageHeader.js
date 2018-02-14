6758534352ecb71fb14bdf9c370da3745e59a8ac
dflo <denisflorkin@gmail.com>
2018-02-14 13:18:01 +0100

    add pageHeader

---
+++ src/Page/PageHeader.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background: #fff;
+  height: 60px;
+  box-sizing: border-box;
+  display: flex;
+  border: 1px solid #D4DAD1;
+  overflow: hidden;
+`;
+
+const TitleContainer = styled.div`
+  flex: 1;
+  display: flex;
+  padding-left: 30px;
+  align-items: center;
+`;
+
+const IconWrapper = styled.div`
+  padding-right: 15px;
+`;
+
+const Title = styled.div`
+  display: inline-block;
+  color: #37373a;
+  font-size: 18px;
+  font-family: 'Fira sans', sans-serif;
+`;
+
+const PageHeader = ({ title, icon, children }) => (
+  <Container>
+    <TitleContainer>
+      {icon && <IconWrapper>
+        {icon}
+      </IconWrapper>}
+      <Title>{title}</Title>
+    </TitleContainer>
+    <div>
+      {children}
+    </div>
+  </Container>
+);
+
+PageHeader.disaplyName = 'PageHeader';
+
+PageHeader.propTypes = {
+  children: PropTypes.node,
+  title: PropTypes.node,
+  icon: PropTypes.node,
+};
+
+export default PageHeader;
--- src/Page/index.js
+++ src/Page/index.js
@@ -1,4 +1,5 @@
 export { default as PageTitle } from './PageTitle';
 export { default as PageWithMenu } from './PageWithMenu';
 export { default as PageSubTitle } from './PageSubTitle';
+export { default as PageHeader } from './PageHeader';
 export { default as Page } from './Page';
