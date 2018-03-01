75c3c9c604bf9e8be300276f98e3cb57de8eaed2
PID <pid@cluedin.com>
2018-03-01 09:54:14 +0100

    Update content with extra

--- src/Layout/ContentWithExtra.js
+++ src/Layout/ContentWithExtra.js
@@ -7,24 +7,19 @@ const ContentWithExtraUI = styled.div`
   min-height: 100%;
   width: 100%;
   overflow: hidden;
-  flex-flow: row wrap;
-
-  & > * {
-    flex-grow: 1;
-    flex-shrink: 0;
-  }
+  flex-flow: ${({ isAfter }) => (isAfter ? 'row-reverse wrap' : 'row wrap')};
 `;
 
 const ContentUI = styled.div`
-  flex-grow: 99;
+  flex:1;
   max-width: 100%;
 `;
 
 const ExtraUI = styled.div`
-  ${({ extraMinWidth }) => extraMinWidth ? `min-width: ${extraMinWidth};` : ''}
+  ${({ extraMinWidth }) => (extraMinWidth ? `min-width: ${extraMinWidth};` : '')}
 `;
 
-const ContentWithExtra = props => {
+const ContentWithExtra = (props) => {
   const {
     extraPosition,
     extra,
@@ -32,20 +27,10 @@ const ContentWithExtra = props => {
     extraMinWidth,
   } = props;
 
-  // default to before
-  const finalChildren = extraPosition === 'after'
-    ? [
-        <ContentUI>{children}</ContentUI>,
-        <ExtraUI extraMinWidth={extraMinWidth}>{extra}</ExtraUI>
-      ]
-    : [
-        <ExtraUI extraMinWidth={extraMinWidth}>{extra}</ExtraUI>,
-        <ContentUI>{children}</ContentUI>
-      ];
-
   return (
-    <ContentWithExtraUI extraPosition={extraPosition}>
-      {finalChildren}
+    <ContentWithExtraUI isAfter={extraPosition === 'after'} extraPosition={extraPosition}>
+      <ExtraUI extraMinWidth={extraMinWidth}>{extra}</ExtraUI>
+      <ContentUI>{children}</ContentUI>
     </ContentWithExtraUI>
   );
 };
@@ -53,14 +38,14 @@ const ContentWithExtra = props => {
 ContentWithExtra.displayName = 'ContentWithExtra';
 
 ContentWithExtra.defaultProps = {
-  extraPosition: "before",
-  extraMinWidth: '200px'
-}
+  extraPosition: 'before',
+  extraMinWidth: '200px',
+};
 
 ContentWithExtra.propTypes = {
   extraPosition: PropTypes.oneOf([
-    'before', 'after'
-  ])
-}
+    'before', 'after',
+  ]),
+};
 
 export default ContentWithExtra;
