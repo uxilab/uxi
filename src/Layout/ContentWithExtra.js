77525c0b740162762f87b44b9fa218e369e14b44
dflo <denisflorkin@gmail.com>
2018-03-05 12:47:56 +0100

    contentWithExtra (no breakpoint)

--- src/Layout/ContentWithExtra.js
+++ src/Layout/ContentWithExtra.js
@@ -7,15 +7,32 @@ const ContentWithExtraUI = styled.div`
   min-height: 100%;
   width: 100%;
   overflow: hidden;
-  flex-flow: ${({ isAfter }) => (isAfter ? 'row-reverse wrap' : 'row wrap')};
+  flex-flow: ${({ isAfter }) => (isAfter ? 'row wrap' : 'row wrap')};
 `;
 
 const ContentUI = styled.div`
   flex:1;
+  /* max-width: 100%;
+  width: 100%;
+  min-width: ${({ contentMinWidth }) => contentMinWidth}; */
+  min-width: 100%;
+  width: ${({ contentMinWidth }) => contentMinWidth};
   max-width: 100%;
+
+  flex-grow: 99;
+  flex-shrink: 0;
+  @media (min-width: 1024px) {
+    width: auto;
+    min-width: ${({ contentMinWidth }) => contentMinWidth};
+  }
 `;
 
 const ExtraUI = styled.div`
+  min-width: 100%;
+  width: ${({ contentMinWidth }) => contentMinWidth};
+  max-width: 100%;
+  flex-grow: 1;
+  flex-shrink: 1;
   ${({ extraMinWidth }) => (extraMinWidth ? `min-width: ${extraMinWidth};` : '')}
 `;
 
@@ -25,12 +42,17 @@ const ContentWithExtra = (props) => {
     extra,
     children,
     extraMinWidth,
+    contentMinWidth,
   } = props;
+
+  const contentItems = [
+    <ExtraUI key="extra" extraMinWidth={extraMinWidth}>{extra}</ExtraUI>,
+    <ContentUI key="content" contentMinWidth={contentMinWidth}>{children}</ContentUI>
+  ]
 
   return (
     <ContentWithExtraUI isAfter={extraPosition === 'after'} extraPosition={extraPosition}>
-      <ExtraUI extraMinWidth={extraMinWidth}>{extra}</ExtraUI>
-      <ContentUI>{children}</ContentUI>
+      { extraPosition === 'after' ? contentItems.reverse() : contentItems }
     </ContentWithExtraUI>
   );
 };
