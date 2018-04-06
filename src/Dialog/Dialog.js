1c597770135cd66afb8ca118234c7b913337eb2c
Denis Florkin <denisflorkin@gmail.com>
2018-04-06 11:56:58 +0200

    make the Dialog responsible for adding Panelcontent appropriate css height rules when Panel is used in dialog mode

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -19,21 +19,42 @@ const DialogStyle = {
   },
 };
 
-const Dialog = ({ show, modal, onClose, children, style }) => (
-  <Modal
-    show={show}
-    onClose={modal ? null : onClose}
-  >
-    <div style={{ ...DialogStyle.root, ...style }}>
-      {children && children.length > 0 && children.map((c, index) => React.cloneElement(c, {
-        key: `dialogContent-${index}`,
-        onClose,
-      }))}
-      {children && !children.length && React.cloneElement(children, {
-        onClose,
-      })}
-    </div>
-  </Modal>
-);
+/**
+ *
+ * If we have a PanelContent in the Dialog, apply dialog style to penl content:
+ * maxHeight: 'calc(80vh - calc( 2 * 50px ))'
+ * This assume panelHeader and PanelFooter are present
+ */
+const Dialog = ({ show, modal, onClose, children, style }) => {
+  const extendedChildren = React.Children.map(children, (c, index) => {
+
+    const childExtendedChildren = React.Children.map(c.props.children, childChildren => {
+      if (childChildren.type.displayName === 'PanelContent') {
+        return React.cloneElement(childChildren, {
+          ...childChildren.props,
+          style: { maxHeight: 'calc(80vh - calc( 2 * 50px ))', ...childChildren.props.style },
+        })
+      }
+      return childChildren
+    })
+
+    return React.cloneElement(c, {
+      key: `dialogContent-${index}`,
+      onClose,
+      children: childExtendedChildren,
+    })
+  })
+
+  return (
+    <Modal
+      show={show}
+      onClose={modal ? null : onClose}
+    >
+      <div style={{ ...DialogStyle.root, ...style }}>
+        {extendedChildren}
+      </div>
+    </Modal>
+  )
+}
 
 export default Dialog;
