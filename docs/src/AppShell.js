9068095b7fa6026b5c0c38253d189919279ecb69
PID <pid@cluedin.com>
2018-05-02 16:59:02 +0200

    Fix appshell

--- docs/src/AppShell.js
+++ docs/src/AppShell.js
@@ -9,60 +9,7 @@ import { AppLayout, Flex, Layout, Col, Row } from 'uxi/Layout';
 import { PageWithMenu } from 'uxi/Page';
 import { ThemedBox } from 'uxi/Box';
 
-const pageWithMenuStyles = { marginTop: '110px', marginLeft: '45px', marginRight: '45px', borderRadius: '5px', padding: '30px 15px', background: '#fff', overflow: 'visible' }
-
-const isActive = (path, currentLocation) =>
-  path.toLowerCase() === currentLocation.toLowerCase();
-
-const makeMenuItem = ({ path, label }, currentLocation) => (
-  <MenuItem isActive={isActive(path, currentLocation)} key={`/components/${path}`}> <Link to={`/components${path.toString()}`}>{label}</Link></MenuItem>
-);
-
-export const routes = [
-  { path: '/font', label: 'Fonts' },
-  { path: '/color', label: 'Colors' },
-  { path: '/alert', label: 'Alert' },
-  { path: '/autocomplete', label: 'AutoComplete' },
-  { path: '/badge', label: 'Badge' },
-  { path: '/breadcrumbs', label: 'BreadCrumbs' },
-  { path: '/box', label: 'Box' },
-  { path: '/button', label: 'Button' },
-  { path: '/classic', label: 'Classic' },
-  { path: '/dashboard', label: 'Dashboard' },
-  { path: '/datagrid', label: 'Data Grid' },
-  { path: '/icons', label: 'Icons' },
-  { path: '/indicator', label: 'Indicator' },
-  { path: '/inputs', label: 'Inputs' },
-  { path: '/internal', label: 'Internal' },
-  { path: '/img', label: 'Image' },
-  { path: '/layouts', label: 'Layout' },
-  { path: '/list', label: 'List' },
-  { path: '/loader', label: 'Loader' },
-  { path: '/menu', label: 'Menu' },
-  // {/* { path: '/sublayout', label: 'SubLayout' }, */ },
-  { path: '/radio', label: 'Radio' },
-  { path: '/sociallinks', label: 'Sociallinks' },
-  { path: '/stepper', label: 'Stepper' },
-  { path: '/switch', label: 'Switch' },
-  { path: '/table', label: 'Table' },
-  { path: '/globalmenu', label: 'Global Menu' },
-  { path: '/widget', label: 'Widget' },
-  { path: '/personalizedmenu', label: 'Personalized Menu' },
-  { path: '/panel', label: 'Panel' },
-  { path: '/gallery', label: 'Gallery' },
-  { path: '/carrousel', label: 'Carrousel' },
-  { path: '/tile', label: 'Tile' },
-  { path: '/Motion', label: 'Motion' },
-]
-
-const ComponentShell = ({ children }) => {
-  const pathname = window.location.pathname;
-  const mainMenu = (
-    <VerticalMenu style={{ borderRight: '1px solid #e9e9e9', height: '100%' }}>
-      {routes.map(x => makeMenuItem(x, pathname)) }
-    </VerticalMenu>
-  );
-
+const Appshell = ({ children }) => {
   return (
     <UXISCThemeProvider>
         <AppLayout>
@@ -79,21 +26,10 @@ const ComponentShell = ({ children }) => {
               </MenuItem>
             </HorizontalMenu>
           </Header>
-          <PageWithMenu style={pageWithMenuStyles} menu={mainMenu} >
-            <Layout>
-              <Row>
-                <Col>
-                  {children}
-                </Col>
-              </Row>
-            </Layout>
-          </PageWithMenu>
-          <ThemedBox isDark>
-            <Flex><H4><strong>uxi</strong></H4></Flex>
-          </ThemedBox>
+            {children}
         </AppLayout>
     </UXISCThemeProvider>
   );
 };
 
-export default ComponentShell;
+export default Appshell;
--- docs/src/app.js
+++ docs/src/app.js
@@ -5,7 +5,7 @@ import { Router, Route } from 'react-router-dom';
 import ComponentRoutes from './ComponentRoutes';
 import createBrowserHistory from 'history/createBrowserHistory';
 import Menu from './Menu';
-
+import Appshell from './Appshell';
 import GetStarted from './GetStarted'
 import Landing from './Landing'
 
@@ -15,11 +15,11 @@ render(
   <Router
     history={createBrowserHistory()}
   >
-    <div>
+    <Appshell>
         <Route exact path="/" component={Landing} />
         <Route exact path="/get-started" component={GetStarted} />
         <Route path="/components" component={ComponentRoutes} />
-    </div>
+    </Appshell>
   </Router>,
   document.getElementById('app'),
 );
