0badfb74c3c42a47ca72f61ab032179001519107
Denis Florkin <denisflorkin@gmail.com>
2018-05-03 11:22:04 +0200

    fix merge mistake

--- docs/src/app.js
+++ docs/src/app.js
@@ -5,7 +5,7 @@ import UXISCThemeProvider from 'uxi/Theme/ThemeProvider/UXISCThemeProvider';
 import ComponentRoutes from './ComponentRoutes';
 import createBrowserHistory from 'history/createBrowserHistory';
 import Menu from './Menu';
-import Appshell from './Appshell';
+import AppShell from './AppShell';
 import GetStarted from './GetStarted'
 import Landing from './Landing'
 import { ThemeProvider  } from 'styled-components';
@@ -18,11 +18,11 @@ render(
   <Router
     history={createBrowserHistory()}
   >
-    <div>
+    <AppShell>
       <Route exact path="/" component={Landing} />
       <Route exact path="/get-started" component={GetStarted} />
       <Route path="/components" component={ComponentRoutes} />
-    </div>
+    </AppShell>
   </Router>
   </UXISCThemeProvider>,
   document.getElementById('app'),
