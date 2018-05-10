678a355592510c5019f5198ec30918036eb434b3
Denis Florkin <denisflorkin@gmail.com>
2018-05-10 14:15:34 +0200

    fix stepper connector

--- src/Stepper/StepConnector.js
+++ src/Stepper/StepConnector.js
@@ -13,8 +13,8 @@ const StepConnectorIndicatorUI = styled.div`
   height: 1px;
   background: lightgrey;
   position: absolute;
-  left: -38%;
-  right: -38%;
+  left: -5%;
+  right: -5%;
 `;
 
 const StepConnector = ({ children }) => (
