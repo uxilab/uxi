ec44c965fe191d2a030b8ab1029d5f1d3826f8fe
Denis Florkin <denisflorkin@gmail.com>
2018-05-16 11:14:37 +0200

    fix typo

--- docs/src/components/pages/PopOver/ExamplePopOverAndPanel.js
+++ docs/src/components/pages/PopOver/ExamplePopOverAndPanel.js
@@ -7,11 +7,11 @@ const main = (<div> click me </div>)
 
 const ExamplePopOverAndPanel = () => (
   <div>
-    <PopOver main={main} anchor={'right'} target={'bottom'}>
+    <PopOver main={main} anchor={'right'}>
       <p style={{ maxWidth: '400px' }}>
         The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness,
       </p>
-      <FlatButton type="primary" text="got it" onClick={function () { this.props.onClose(); console.log('ok', arguments) }} />
+      <FlatButton type="primary" text="got it" onClick={() => console.log('ok', arguments) } />
     </PopOver>
   </div>
 );
