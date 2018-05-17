d71a396656c064e541eae994cecae0a0516005b2
Denis Florkin <denisflorkin@gmail.com>
2018-05-17 22:06:01 +0200

    clean popover of the popoverbody

--- src/internal/PopOver.js
+++ src/internal/PopOver.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import DropDown from '../internal/DropDown';
-import PopOverBody from './PopOverBody';
 
 const PopOver = (props) => {
   'r';
-  const { main, children, anchor, target } = props;
+  const {
+    main,
+    children,
+    anchor,
+    target,
+    itemsStyle,
+  } = props;
 
   return (
     <DropDown
-      items={(
-        <PopOverBody>
-          {children}
-        </PopOverBody>
-      )}
+      itemsStyle={{ zIndex: 150, ...itemsStyle }}
+      items={children}
       main={main}
       isPopOver
       anchor={anchor}
