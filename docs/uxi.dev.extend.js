4badf8c696f17136c853272d36befe17ffcf3635
Denis Florkin <denisflorkin@gmail.com>
2018-05-17 20:06:24 +0200

    latest uxi-cli

--- docs/uxi.dev.extend.js
+++ docs/uxi.dev.extend.js
@@ -7,12 +7,20 @@ module.exports = (config) => {
     exclude: /node_modules/,
   });
 
+  console.log('config.devServer.port', config.devServer.port)
+  console.log('config.entry[1]', config.entry[1])
+  config.devServer.port = 8989;
+  config.entry[1].replace(/(:\d*)$/, ':8989');
+
   config.resolve.alias = {
     uxi: path.resolve(__dirname, '../src'),
     'styled-components': path.resolve(__dirname, 'node_modules/styled-components'),
     'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     'react': path.resolve(__dirname, 'node_modules/react'),
   };
+
+  console.log('config.entry[1]', config.entry[1])
+  console.log('config.devServer.port', config.devServer.port)
 
   return config;
 };
