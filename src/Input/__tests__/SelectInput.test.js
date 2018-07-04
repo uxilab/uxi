13d3f9245949735092fdeab1cd7e42cf5baf0fea
Denis Florkin <denisflorkin@gmail.com>
2018-07-04 10:01:52 +0200

    add test file :
    but no actual test because after an hour trying to figure out why this test won't run I give up

---
+++ src/Input/__tests__/SelectInput.test.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import SelectInput from '../SelectInput'; // eslint-disable-line
+
+describe('<SelectInput />', () => {
+  it('it cannot have test apparently for some reason, try it and let me know, just uncomment line 2 and unr the test -df', () => {
+    expect(true).toEqual(true);
+  });
+  // it('exists', () => {
+  //   expect(SelectInput).toBeDefined();
+  // });
+
+  // it('matches snapshot', () => {
+  //   const sut = shallow(
+  //     <SelectInput />
+  //   );
+  //   expect(sut).toMatchSnapshot();
+  // });
+});
