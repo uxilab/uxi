d8d7310cfa30086b5926cedc47309e6edbe77e89
Denis Florkin <denisflorkin@gmail.com>
2018-09-19 19:10:28 +0200

    make `buttonResetStylesCSSString` available to consumer
    but not promoted to index of Button

---
+++ src/Button/buttonResetStyles.js
@@ -0,0 +1,22 @@
+import { css } from 'styled-components';
+
+export const buttonResetstyles = {
+  border: 'none',
+  padding: 0,
+  fontSize: 'inherit',
+  background: 'transparent',
+  borderRadius: 0,
+};
+
+export const buttonResetStylesCSSString = css`
+  border: none;
+  padding: 0;
+  font-size: inherit;
+  background: transparent;
+  border-radius: 0;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+    /* box-shadow: 0 0 10px #719ECE; */
+  }
+`.join('\n');
