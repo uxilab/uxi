4abb09828bc9420b23d20c150c525645044c4266
Denis Florkin <denisflorkin@gmail.com>
2018-10-30 12:44:11 +0100

    imporve TextEllipsis:
    - add `truncateHead` props to truncate the string the left (default to truncating tail)

--- src/Text/TextEllipsis.js
+++ src/Text/TextEllipsis.js
@@ -1,23 +1,41 @@
+import React from 'react';
 import styled from 'styled-components';
 
+export const testEllipsisStylesCSSString = `
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
+`;
 
 const TextEllipsis = styled.div.attrs({
   title: ({ children, title }) => {
     if (title) {
       if (typeof title === 'string') {
-        return title;
+        return JSON.stringify(title); // not safe to ad dynamic code in html attriubtes
       }
     }
     if (children && typeof children === 'string') {
-      return children;
+      return JSON.stringify(title); // not safe to ad dynamic code in html attriubtes
     }
     return 'no title';
   },
+  children: ({ children, truncateHead }) => (truncateHead
+    ? [children, () => (<span>{`${children}`}&nbsp;</span>)]
+    : children),
 })`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   max-width: 100%;
+  /* truncate tail by default */
+  ${({ truncateHead }) => (truncateHead ? 'text-align: left; direction: rtl;' : '')};
+  ${({ truncateHead }) => (truncateHead && `
+    &:after {
+      content: ' ';
+      display: block;
+    }
+  `)};
 `;
 
 export default TextEllipsis;
