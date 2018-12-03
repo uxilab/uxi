dc82e4600db7dfdbcf1e7a2e72c61d8de5ee2115
PID <pid@cluedin.com>
2018-12-03 08:29:29 +0100

    Fix lint errors

--- src/Input/Tree.js
+++ src/Input/Tree.js
@@ -17,48 +17,6 @@ const ToogleLink = styled.a`
   align-items: center;
 `;
 
-const searchTree = (element, predicate) => {
-  if (predicate(element)) {
-    return element;
-  }
-
-  let result = null;
-  (element.childNodes || []).forEach((child) => {
-    if(!result) {
-      result = searchTree(child, predicate);
-    }
-  });
-
-  return result;
-};
-
-const updateTree = (node, checked, selectedId) => {
-  const foundNode = searchTree(node, (n) => n.Id === selectedId);
-
-  //when false we chould update the tree backwards
-  foundNode.isChecked = checked;
-
-  if(foundNode.childNodes) {
-    forEachTreeElement(
-      foundNode,
-      (n) => {
-        n.isChecked = checked;
-      }
-    );
-  }
-
-  return node;
-};
-
-const getUpdatedSelection = (node) => {
-  
-};
-const arrayToTree = (arr, parentId = null) => {
-  return (!arr || !arr.filter) ? [] : arr
-    .filter(n => n.Parent === parentId)
-    .map(n => ({ ...n, childNodes: arrayToTree(arr, n.Id) }));
-}
-
 const forEachTreeElement = (tree, cb, predicate) => {
   if (tree.childNodes) {
     tree.childNodes.forEach((n) => {
@@ -69,13 +27,55 @@ const forEachTreeElement = (tree, cb, predicate) => {
   if ((!predicate || predicate(tree)) && cb) {
     cb(tree);
   }
-}
+};
+
+const searchTree = (element, predicate) => {
+  if (predicate(element)) {
+    return element;
+  }
+
+  let result = null;
+  (element.childNodes || []).forEach((child) => {
+    if (!result) {
+      result = searchTree(child, predicate);
+    }
+  });
+
+  return result;
+};
+
+const updateTree = (node, checked, selectedId) => {
+  const foundNode = searchTree(node, n => n.Id === selectedId);
+
+  // when false we chould update the tree backwards
+  foundNode.isChecked = checked;
+
+  if (foundNode.childNodes) {
+    forEachTreeElement(
+      foundNode,
+      (n) => {
+        // eslint-disable-next-line no-param-reassign
+        n.isChecked = checked;
+      }
+    );
+  }
+
+  return node;
+};
+
+const getUpdatedSelection = () => {
+
+};
+
+const arrayToTree = (arr, parentId = null) => ((!arr || !arr.filter) ? [] : arr
+  .filter(n => n.Parent === parentId)
+  .map(n => ({ ...n, childNodes: arrayToTree(arr, n.Id) })));
 
 class TreeNode extends Component {
   static propTypes = {
     isChecked: PropTypes.bool.isRequired,
     onSelect: PropTypes.func.isRequired,
-    node: PropTypes.shape({
+    defaultNode: PropTypes.shape({
       Id: PropTypes.string.isRequired,
       Name: PropTypes.string.isRequired,
       IsChecked: PropTypes.bool,
@@ -94,39 +94,74 @@ class TreeNode extends Component {
     this.select = this.select.bind(this);
     this.toggle = this.toggle.bind(this);
     this.onSelectElement = this.onSelectElement.bind(this);
-
-  }
-  onTreeSelectNode(name, tree, valuesFromTree) {
-    const currentValues = this.props.feedValues[name];
-    const checkedStates = {};
-
-    forEachTreeElement(
-      valuesFromTree.node,
-      n => n.id === valuesFromTree.node.id,
-      nn => checkedStates[nn.Id] = valuesFromTree.isChecked //eslint-disable-line
-    );
-
-    const nextValues = currentValues.map(cv => ({ ...cv, IsChecked: !!checkedStates[cv.Id] }));
-
   }
 
 
-  toggle() {
-    this.setState({ visible: !this.state.visible });
-  }
-
-  select (currentNode, isChecked) {
+  onSelectElement(selectedNode, isChecked) {
     const { node } = this.state;
     const { onSelect } = this.props;
 
+    const updateNodes = updateTree(node, isChecked, selectedNode.Id);
+    const selectedNodes = getUpdatedSelection(updateNodes) || [];
+
+    this.setState({
+      node: updateNodes,
+      selectedNodes,
+    });
+
     if (onSelect) {
-      onSelect(
-        currentNode,
-        isChecked,
-      );
+      onSelect(selectedNodes);
     }
   }
 
+  getNodeContent() {
+    const { node, visible } = this.state;
+    const { onSelectElement, isChild } = this.props;
+
+    const nodeValue = node.isChecked || false;
+    const title = node.title || node.Name;
+    const expanderIconContent = this.getToggleLink();
+
+    if (visible) {
+      return (
+        <TreeNodeContainer>
+          {expanderIconContent}
+          <Checkbox
+            checked={nodeValue}
+            onChange={
+              (e, isChecked) => {
+                if (isChild) {
+                  onSelectElement(node, isChecked);
+                } else {
+                  this.onSelectElement(node, isChecked);
+                }
+              }
+            }
+            label={title}
+          />
+        </TreeNodeContainer>
+      );
+    }
+
+    return (
+      <TreeNodeContainer>
+        {expanderIconContent}
+        <Checkbox
+          checked={nodeValue}
+          onChange={
+            (e, isChecked) => {
+              if (isChild) {
+                onSelectElement(node, isChecked);
+              } else {
+                this.onSelectElement(node, isChecked);
+              }
+            }
+          }
+          label={title}
+        />
+      </TreeNodeContainer>
+    );
+  }
   getToggleLink() {
     const { visible, node } = this.state;
 
@@ -142,85 +177,43 @@ class TreeNode extends Component {
       <ToogleLink onClick={this.toggle}>
         {
           visible ? (
-          <Arrowdown size={16} style={{ paddingLeft: '5px' }} />
-          ): (
-          <Arrowright size={16} style={{ paddingLeft: '5px' }} />
+            <Arrowdown size={16} style={{ paddingLeft: '5px' }} />
+          ) : (
+            <Arrowright size={16} style={{ paddingLeft: '5px' }} />
           )
         }
       </ToogleLink>
     );
   }
 
-  getNodeContent() {
-    const { node, visible } = this.state;
-    const { onSelectElement, isChild } = this.props;
-
-    const nodeValue = node.isChecked || false;
-    const title = node.title || node.Name;
-    const expanderIconContent = this.getToggleLink();
-
-    if (visible) {
-     return (
-        <TreeNodeContainer>
-          {expanderIconContent}
-          <Checkbox
-            checked={nodeValue}
-            onChange={(e, isChecked) => { isChild ? onSelectElement(node, isChecked) : this.onSelectElement(node, isChecked); }}
-            label={title}
-          />
-        </TreeNodeContainer>
-      );
-    }
-
-    return (
-      <TreeNodeContainer>
-        {expanderIconContent}
-        <Checkbox
-          checked={nodeValue}
-          onChange={(e, isChecked) => { isChild ? onSelectElement(node, isChecked) : this.onSelectElement(node, isChecked); }}
-          label={title}
-        />
-      </TreeNodeContainer>
-    );
-  }
-
-  onSelectElement(selectedNode, isChecked) {
-    alert('test')
-    const { node } = this.state;
+  select(currentNode, isChecked) {
     const { onSelect } = this.props;
 
-    const updateNodes = updateTree(node, isChecked, selectedNode.Id);
-    const selectedNodes = getUpdatedSelection(updateNodes) || [];
-
-    this.setState({
-      node: updateNodes,
-      selectedNodes,
-    });
-
-    if(onSelect) {
-      onSelect(selectedNodes);
+    if (onSelect) {
+      onSelect(
+        currentNode,
+        isChecked,
+      );
     }
+  }
+
+  toggle() {
+    this.setState({ visible: !this.state.visible });
   }
 
   render() {
     const {
-      onSelect,
       className,
       style,
     } = this.props;
     const {
       node,
-      visible
+      visible,
     } = this.state;
 
-    let childNodes;
-    let content;
-
-    if(!node) {
+    if (!node) {
       return (
-        <div className={className} style={style || {}}>
-          {content}
-        </div>
+        <div className={className} style={style || {}} />
       );
     }
 
@@ -236,7 +229,7 @@ class TreeNode extends Component {
           (
             <ul style={{ marginLeft: '30px' }}>
               {
-                 node.childNodes.map(
+                node.childNodes.map(
                   (n, index) => (
                     <li key={index}>
                       <TreeNode
