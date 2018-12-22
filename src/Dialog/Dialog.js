7b7d8f3bc8a34818d432db45bb1beeb7d09a39a1
Denis Florkin <denisflorkin@gmail.com>
2018-12-22 12:18:41 +0100

    Improve Dialog
    introduce new props `isFullWidth` and `isfullScreen` on Dialog:
    - require less medling with granular style control
    - while still allowing it if need be

--- src/Dialog/Dialog.js
+++ src/Dialog/Dialog.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import styled from 'styled-components';
 import Modal from '../internal/Modal';
 
-const DialogStyle = {
-  root: {
-    zIndex: '100',
-    position: 'fixed',
-    top: '10vh',
-    width: 'calc(100% - 20vw)',
-    maxWidth: '700px',
-    maxHeight: '80vh',
-    margin: '0 auto',
-    bottom: 'auto',
-    left: '10vw',
-    right: '10vw',
-    backfaceVisibility: 'hidden',
-    background: '#fff',
-    borderRadius: '2px',
-  },
-};
+const DialogUI = styled.div`
+  z-index: 100;
+  position: fixed;
+  top: 10vh;
+  width: calc(100% - 20vw);
+  max-width: 700px;
+  max-height: 80vh;
+  margin: 0 auto;
+  bottom: auto;
+  left: 10vw;
+  right: 10vw;
+  backface-visibility: hidden;
+  background: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+  box-shadow:
+    rgba(0, 0, 0, 0.5) 0px 1px 6px,
+    rgba(0, 0, 0, 0.2) 0px 4px 16px;
+  box-shadow:
+    rgba(0, 0, 0, 0.3) 0px 2px 21px 1px,
+    rgba(0,0,0,0.2) 0px 1px 22px 4px;
+
+  ${({ isFullWidth }) => isFullWidth && `
+    max-width: 80vw;
+  `}
+  ${({ isFullScreen }) => isFullScreen && `
+    max-width: 92vw;
+    width: calc(100% - 8vw);
+    max-height: 92vh;
+    left: 4vw;
+    right: 4vw;
+    top: 4vh;
+  `}
+`;
 
 /**
  *
@@ -25,53 +43,62 @@ const DialogStyle = {
  * maxHeight: 'calc(80vh - calc( 2 * 50px ))'
  * This assume panelHeader and PanelFooter are present
  */
-class Dialog extends Component {
-  render() {
-    const { show, modal, onClose, children, style } = this.props;
+const Dialog = ({
+  show,
+  modal,
+  onClose,
+  children,
+  style,
+  isFullWidth,
+  isFullScreen,
+}) => {
+  const extendedChildren = React.Children.map(children, (c, index) => {
+    const childExtendedChildren = React.Children.map(c.props.children, (childChildren) => {
+      if (childChildren
+        && childChildren.type
+        && childChildren.type.displayName === 'PanelContent'
+      ) {
+        let baseHeight = '80vh';
+        if (isFullScreen) { baseHeight = '92vh'; }
 
-    const extendedChildren = React.Children.map(children, (c, index) => {
-      const childExtendedChildren = React.Children.map(c.props.children, (childChildren) => {
-        if (childChildren
-          && childChildren.type
-          && childChildren.type.displayName === 'PanelContent'
-        ) {
-          return React.cloneElement(childChildren, {
-            ...childChildren.props,
-            style: {
-              maxHeight: 'calc(80vh - (2 * 54px))', // for panel header and footer
-              ...childChildren.props.style,
-            },
-          });
-        }
-        return childChildren;
-      });
-
-      const shadowRuleIfChildIsPanel = (c && c.type && c.type.displayName === 'Panel')
-        ? { boxShadow: 'rgba(0, 0, 0, 0.5) 0px 1px 6px, rgba(0, 0, 0, 0.2) 0px 1px 32px' }
-        : {};
-
-      return React.cloneElement(c, {
-        key: `dialogContent-${index}`,
-        onClose,
-        style: {
-          ...(shadowRuleIfChildIsPanel),
-          ...((c && c.props && c.props.style) ? c.props.style : {}),
-        },
-        children: childExtendedChildren,
-      });
+        return React.cloneElement(childChildren, {
+          ...childChildren.props,
+          style: {
+            maxHeight: `calc(${baseHeight} - (2 * 54px))`, // for panel header and footer
+            ...childChildren.props.style,
+          },
+        });
+      }
+      return childChildren;
     });
 
-    return (
-      <Modal
-        show={show}
-        onClose={modal ? null : onClose}
-      >
-        <div style={{ ...DialogStyle.root, ...style }}>
-          {extendedChildren}
-        </div>
-      </Modal>
-    );
-  }
-}
+    return React.cloneElement(c, {
+      key: `dialogContent-${index}`,
+      onClose,
+      style: {
+        ...((c && c.props && c.props.style) ? c.props.style : {}),
+      },
+      children: childExtendedChildren,
+    });
+  });
+
+  return (
+    <Modal
+      show={show}
+      onClose={modal ? null : onClose}
+    >
+      <DialogUI style={style} isFullScreen={isFullScreen} isFullWidth={isFullWidth}>
+        {extendedChildren}
+      </DialogUI>
+    </Modal>
+  );
+};
+
+Dialog.displayName = 'Dialog';
+
+Dialog.defaultProps = {
+  isFullScreen: false,
+  isFullWidth: false,
+};
 
 export default Dialog;
