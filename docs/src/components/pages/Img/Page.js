f7f301c5a238121a21cccd6453d8f3bbb9aaa528
Denis Florkin <denisflorkin@gmail.com>
2018-12-23 09:52:43 +0100

    Img doc

--- docs/src/components/pages/Img/Page.js
+++ docs/src/components/pages/Img/Page.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ExampleSimple from './ExampleSimple';
+import { H1 } from 'uxi/Classic';
+import CodeExample from '../../CodeExample';
+
+import ImgExample from './ImgExample';
+import RAWImgExample from '!raw-loader!./ImgExample';
+import RAWMDImgExample from '!raw-loader!./ImgExample.md';
 
 const BoxPage = () => (
-  <div>
-    <ExampleSimple />
+ <div>
+  <H1>Panel</H1>
+    <ul>
+      <li>
+        <CodeExample
+          code={RAWImgExample}
+          component
+          title="Dialog"
+          description={RAWMDImgExample}
+          hasPadding
+        >
+          <ImgExample />
+        </CodeExample>
+      </li>
+    </ul>
   </div>
 );
 
