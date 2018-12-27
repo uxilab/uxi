0bfe9626b5e40a03998115d6d32df322b5b9d0e8
Denis Florkin <denisflorkin@gmail.com>
2018-12-27 18:59:38 +0100

    wip save add unstyledButton (use it to solve focus style issue in inputgroup and select input + ripple

---
+++ src/Button/UnstyledButton1.js
@@ -0,0 +1,15 @@
+import { ButtonComponent } from './ButtonComponent';
+import {
+  UnstyledButtonBaseMixin,
+} from './UnstyledButtonBaseMixin';
+
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class UnstyledButton extends ButtonComponent {
+  constructor(props) {
+    super(props);
+    this.ButtonBaseMixin = UnstyledButtonBaseMixin;
+  }
+}
+
+export default UnstyledButton;
