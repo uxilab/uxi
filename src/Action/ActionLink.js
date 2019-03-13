940351a0c54e6c7ee1c213c42bcacaf8a07577bc
Denis Florkin <denisflorkin@gmail.com>
2019-03-13 16:12:21 +0100

    add ActionLink:
    support new dashboards

---
+++ src/Action/ActionLink.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import styled from 'styled-components';
+import Widget from '../Widget'; // eslint-disable-line import/no-named-as-default
+/*
+const getTypeColor = ({ palette }, type) => {
+  if (palette.semantic[type]) { return palette.semantic[type]; }
+  if (type === 'primary') { return palette.accent.main; }
+  if (type === 'secondary') { return palette.primary.main; }
+  return '#fff';
+};
+
+const getLightTypeColor = ({ palette }, type) => {
+  if (palette.semantic[type]) { return palette.semantic[type]; }
+  if (type === 'primary') { return palette.accent.light; }
+  if (type === 'secondary') { return palette.primary.light; }
+  return '#fff';
+};
+ */
+
+const ActionWrapper = styled.div`
+  cursor: pointer;
+  width: 100%;
+  position: relative;
+  min-height: 180px;
+  display: flex;
+  align-items: left;
+  justify-content: center;
+  border-top-width: 0;
+  border-left-width: 0;
+  border-right-width: 0;
+  border-bottom-width: 5px;
+  border-style: solid;
+  border-color: currentColor;
+  padding: 24px;
+  flex-direction: column;
+  box-sizing: border-box;
+`;
+
+const ActionIconWrapper = styled.div`
+  svg {
+    width: 60px !important;
+    height:60px !important;
+    fill: currentColor
+  }
+`;
+
+const ActionDisplayNameWrapper = styled.div`
+  color: currentColor;
+  font-weight: 600;
+  font-size: 16px;
+  font-family: 'Open sans', sans-serif;
+  margin-top:20px;
+  flex-grow: 99;
+  display: flex;
+  align-items: center;
+`;
+
+const Action = ({
+  menuDescriptor,
+  style,
+  Link = () => null,
+  to = '#',
+}) => (
+  <Widget>
+    <Link to={to}>
+      <ActionWrapper
+        onClick={menuDescriptor.onClick}
+        style={style}
+      >
+        <ActionIconWrapper>
+          {menuDescriptor.icon}
+        </ActionIconWrapper>
+        <ActionDisplayNameWrapper>
+          {menuDescriptor.displayName}
+        </ActionDisplayNameWrapper>
+      </ActionWrapper>
+    </Link>
+  </Widget>
+);
+
+export default Action;
--- src/Widget/index.js
+++ src/Widget/index.js
@@ -2,5 +2,7 @@ import Widget from './Widget';
 
 export default Widget;
 
-export Widget from './Widget';
 export WidgetSubHeader from './WidgetSubHeader';
+
+// TODO deprecate the non default export of Widget
+export Widget from './Widget';
