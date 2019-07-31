71df11504110d9ae977c2f25fa2fcc38f6f7bd43
Denis Florkin <denisflorkin@gmail.com>
2019-07-31 13:20:58 +0200

    safer getAppropriateIcon

--- src/Icons/getAppropriateIcon.js
+++ src/Icons/getAppropriateIcon.js
@@ -2,7 +2,7 @@ import * as Icons from './index';
 
 const capitalize = x => x.charAt(0).toUpperCase() + x.slice(1);
 
-export const getAppropriateIcon = (identifier) => {
+export const getAppropriateIcon = (identifier = '') => {
   const cleanedIdentifer = capitalize(identifier.toLowerCase());
   return Icons[cleanedIdentifer] ? Icons[cleanedIdentifer] : Icons.Circle;
 };
