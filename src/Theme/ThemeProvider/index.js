a92821b02c0bd686a07d410a7e65ec6af11a0113
denisflorkin <denisflorkin@gmail.com>
2020-01-15 15:37:46 +0100

    fix(font loadeing repeatedly) :
    maybe let's see

--- src/Theme/ThemeProvider/index.js
+++ src/Theme/ThemeProvider/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, { ThemeProvider as SCThemeProvider } from 'styled-components';
-import { makeGlobalCSSInjector } from '../global';
+import { ThemeProvider as SCThemeProvider } from 'styled-components';
+import { GlobalStyle } from '../global';
 import { getThemeWithCustomPalette, mergeTheme } from '../utils';
 import { theme as UXITheme } from '../index';
 import { ThemeProvider as UXIContextThemeProvider } from './ContextThemeProvider';
-
-const GlobalStyles = styled.div`
-  & {}
-`;
 
 const UXISCThemeProvider = (props) => {
   const {
@@ -25,18 +21,15 @@ const UXISCThemeProvider = (props) => {
   } else if (customPalette) {
     theTheme = getThemeWithCustomPalette(customPalette);
   }
-  const actualCSSString = makeGlobalCSSInjector(theTheme);
 
   return (
     <SCThemeProvider theme={theTheme || theme} >
-      <GlobalStyles data-global-styles>
-        <UXIContextThemeProvider>
-          <div style={{ height: '100%' }}>
-            <style dangerouslySetInnerHTML={{ __html: actualCSSString }} />
-            {children}
-          </div>
-        </UXIContextThemeProvider>
-      </GlobalStyles>
+      <UXIContextThemeProvider>
+        <div style={{ height: '100%' }}>
+          <GlobalStyle />
+          {children}
+        </div>
+      </UXIContextThemeProvider>
     </SCThemeProvider>
   );
 };
--- src/Theme/global.js
+++ src/Theme/global.js
@@ -1,3 +1,4 @@
+import { createGlobalStyle } from 'styled-components';
 // import { css } from 'styled-components';
 import { lighten } from './colorManipulator';
 /** those are css`` mixins import */
@@ -19,95 +20,181 @@ import simpleLayout from './simpleLayout'; // eslint-disable-line import/no-name
 /* eslint-disable indent */
 /* eslint-disable prefer-template */
 
-export const makeGlobalCSSInjector = (theme) => {
-  if (!theme) return () => console.warn('called `makeGlobalCSSInjector` without passing a theme');
-  const {
-    palette,
-    transition,
-  } = theme;
+// export const makeGlobalCSSInjector = (theme) => {
+//   if (!theme) return () => console.warn('called `makeGlobalCSSInjector` without passing a theme');
+//   const {
+//     palette,
+//     transition,
+//   } = theme;
 
-  return `
-    ${markdown}
-    ${fonts}
-    ${tooltip}
+//   return `
+//     ${markdown}
+//     ${fonts}
+//     ${tooltip}
 
-    * {
-      scroll-behavior: smooth; /* is not an inherited prop */
-    }
+//     * {
+//       scroll-behavior: smooth; /* is not an inherited prop */
+//     }
 
-    html {
-      min-height: 100%;
-      margin: 0;
-      padding: 0;
-      background: ${palette.white};
-      font-family: 'Open Sans', sans-serif;
-      font-weight: 400;
-    }
-    body {
-      min-height: 100%;
-      margin: 0;
-      padding: 0;
-      font-size: 14px;
-      color: ${palette.darkGrey};
-    }
-    ::selection {
-      color: white;
-      /* text-decoration-color: white; */
-      background-color: rgba(0, 137, 122, 0.9); /* theme.accent.dark for contrast */
-    }
+//     html {
+//       min-height: 100%;
+//       margin: 0;
+//       padding: 0;
+//       background: ${palette.white};
+//       font-family: 'Open Sans', sans-serif;
+//       font-weight: 400;
+//     }
+//     body {
+//       min-height: 100%;
+//       margin: 0;
+//       padding: 0;
+//       font-size: 14px;
+//       color: ${palette.darkGrey};
+//     }
+//     ::selection {
+//       color: white;
+//       /* text-decoration-color: white; */
+//       background-color: rgba(0, 137, 122, 0.9); /* theme.accent.dark for contrast */
+//     }
 
-    button::-moz-focus-inner {
-      border: 0;
-    }
+//     button::-moz-focus-inner {
+//       border: 0;
+//     }
 
-    /** Links */
-    a {
-      color: ${palette.accent.main};
-      text-decoration: none;
-      cursor: pointer;
-    }
-    a:hover {
-      color: ${lighten(palette.accent.main, 0.1)};
-      text-decoration: underline;
-    }
-
-
-    /** Headings */
-    h1, h2, h3, h4 {
-      color: currentColor;
-    }
+//     /** Links */
+//     a {
+//       color: ${palette.accent.main};
+//       text-decoration: none;
+//       cursor: pointer;
+//     }
+//     a:hover {
+//       color: ${lighten(palette.accent.main, 0.1)};
+//       text-decoration: underline;
+//     }
 
 
-    /** UL */
-    ul {
-      margin: 0;
-      padding: 0;
-      list-style: none;
-    }
+//     /** Headings */
+//     h1, h2, h3, h4 {
+//       color: currentColor;
+//     }
 
 
-    /** SVG */
-    svg {
-      transition: ${transition.defaultAll};
-    }
-    svg.uxi_svg-icon-wrapper {
-      transition: none;
-    }
-    svg.uxi_svg-icon-wrapper svg {
-      transition: ${transition.defaultAll};
-    }
-    /* little trick relating to SvgIcon: a svg wrapper */
-    // svg>svg { fill: inherit; }
+//     /** UL */
+//     ul {
+//       margin: 0;
+//       padding: 0;
+//       list-style: none;
+//     }
 
 
-    /**
-     * Extend using css from styled-components
-     */
-    ${grid}
-    ${layout}
-    ${simpleLayout}
-  `;
-  // return injectGlobal`${getGlobaStyles(theme)}`
-};
+//     /** SVG */
+//     svg {
+//       transition: ${transition.defaultAll};
+//     }
+//     svg.uxi_svg-icon-wrapper {
+//       transition: none;
+//     }
+//     svg.uxi_svg-icon-wrapper svg {
+//       transition: ${transition.defaultAll};
+//     }
+//     /* little trick relating to SvgIcon: a svg wrapper */
+//     // svg>svg { fill: inherit; }
 
-export default makeGlobalCSSInjector;
+
+//     /**
+//      * Extend using css from styled-components
+//      */
+//     ${grid}
+//     ${layout}
+//     ${simpleLayout}
+//   `;
+//   // return injectGlobal`${getGlobaStyles(theme)}`
+// };
+
+// export default makeGlobalCSSInjector;
+export default null;
+
+export const GlobalStyle = createGlobalStyle`
+  ${markdown}
+  /* ${fonts} */
+  ${tooltip}
+
+  * {
+    scroll-behavior: smooth; /* is not an inherited prop */
+  }
+
+  html {
+    min-height: 100%;
+    margin: 0;
+    padding: 0;
+    background: ${({ theme: { palette } }) => palette.white};
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 400;
+  }
+  body {
+    min-height: 100%;
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    color: ${({ theme: { palette } }) => palette.darkGrey};
+  }
+  ::selection {
+    color: white;
+    /* text-decoration-color: white; */
+    background-color: rgba(0, 137, 122, 0.9); /* theme.accent.dark for contrast */
+  }
+
+  button::-moz-focus-inner {
+    border: 0;
+  }
+
+  /** Links */
+  a {
+    color: ${({ theme: { palette } }) => palette.accent.main};
+    text-decoration: none;
+    cursor: pointer;
+  }
+  a:hover {
+    color: ${({ theme: { palette } }) => lighten(palette.accent.main, 0.1)};
+    text-decoration: underline;
+  }
+
+
+  /** Headings */
+  h1, h2, h3, h4 {
+    color: currentColor;
+  }
+
+
+  /** UL */
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+
+
+  /** SVG */
+  svg {
+    transition: ${({ theme: { transition } }) => transition.defaultAll};
+  }
+  svg.uxi_svg-icon-wrapper {
+    transition: none;
+  }
+  svg.uxi_svg-icon-wrapper svg {
+    transition: ${({ theme: { transition } }) => transition.defaultAll};
+  }
+  /* little trick relating to SvgIcon: a svg wrapper */
+  // svg>svg { fill: inherit; }
+
+
+  /**
+    * Extend using css from styled-components
+    */
+  ${grid}
+  ${layout}
+  ${simpleLayout}
+`;
+
+
+console.log('GlobalStyle in its file', GlobalStyle);
